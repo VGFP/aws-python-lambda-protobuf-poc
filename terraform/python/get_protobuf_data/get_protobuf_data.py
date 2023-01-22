10da51b3a46ca6d61ce72faf4d4dd1678d6f8110
VGFP <pierwiastekz3@interia.pl>
2023-01-22 16:00:53 +0100

    printing deleted from get lambdas

--- terraform/python/get_protobuf_data/get_protobuf_data.py
+++ terraform/python/get_protobuf_data/get_protobuf_data.py
@@ -5,7 +5,5 @@ import base64
 def lambda_handler(event, context):
     obj = message_pb2.MyMessage()
     obj.ParseFromString(base64.b64decode(event["Records"][0]["body"]))
-    for name in obj.DESCRIPTOR.fields_by_name:
-        print(f"{name}: {getattr(obj, name)}")
 
     return {"statusCode": 200}
