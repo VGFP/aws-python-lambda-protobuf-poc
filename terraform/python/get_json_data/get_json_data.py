c0f5210cbe9ff9e6d539fc5bc04f02bd7b9a6820
VGFP <pierwiastekz3@interia.pl>
2023-01-23 18:33:38 +0100

    Added message size comparison

--- terraform/python/get_json_data/get_json_data.py
+++ terraform/python/get_json_data/get_json_data.py
@@ -1,6 +1,7 @@
 import json
-
+import sys
 
 def lambda_handler(event, context):
+    print(f'Received message size: {sys.getsizeof(event["Records"][0]["body"])}')
     obj = json.loads(event["Records"][0]["body"])
     return {"statusCode": 200}
--- terraform/python/get_protobuf_data/get_protobuf_data.py
+++ terraform/python/get_protobuf_data/get_protobuf_data.py
@@ -1,9 +1,9 @@
 import large_message_pb2
 import base64
-
+import sys
 
 def lambda_handler(event, context):
-
+    print(f'Received message size: {sys.getsizeof(event["Records"][0]["body"])}')
     protobuf_obj = large_message_pb2.SomeMessage()
     protobuf_obj.ParseFromString(base64.b64decode(event["Records"][0]["body"]))
 
